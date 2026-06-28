f72da8bdc6932a67ca8b4ca9b4abae82c29e6cdf
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-28 09:26:14 -0400

    Add contracts index

---
+++ contracts/index.js
@@ -0,0 +1,4 @@
+export { KitStatus, isKitStatus } from "./kit-status.js";
+export { KitKind, isKitKind } from "./kit-kind.js";
+export { KitStability, isKitStability } from "./kit-stability.js";
+export { DomainKind, isDomainKind } from "./domain-kind.js";
